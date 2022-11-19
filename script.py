69989fbc1b4a89e1532aaf13fb858efbeb75b7bf
Giovanni Enrico Loni <65892886+genricoloni@users.noreply.github.com>
2022-11-19 16:01:18 +0100

    now in english!

--- script.py
+++ script.py
@@ -1,35 +1,43 @@
-#funzione che raccoglie una listra di nomi, la duplica e assegna un nome ad un altro
 import random
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+
 def fun():
-    x = input("Quante persone ")
+    x = input("How much friends")
     x = int(x)
 
-    y = list(range(x))
-
+    #create a list, one for iterator and one for backup
+    y  = list(range(x))
     back = list(range(x))
-
+    #fill the list with your friend's names
     for i in range(x):
         y[i] = input()
         back[i] = y[i]
 
     z = list(range(x))
 
+    # count is the number of friends yet to be paired
     count = x
 
-    print(count)
-
+    print("DONOR:     RECEIVER:")
     for i in range(x):
+        # random index from 0 to count
         w = random.randrange(0,count)
         if z[i] == y[w]:
+            #if these two are the same, there'll be a self gift and that would be bad
             count += 1
+            #so we'll just skip this iteration
             continue
+        #assign the value to the list
         z[i] = y[w]
-        
+        # print the donor and receiver
         print(back[i], "->",z[i])
+        #remove friend from the list
         y.remove(y[w])
+        # count is decremented
         count -= 1
-        
-        
 
 
 fun()
